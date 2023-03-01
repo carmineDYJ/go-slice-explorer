0c03025c4a0023f7e4361983dd20cc268d6606d5
carminedyj <dingyujie@ztgame.com>
2023-03-01 19:50:11 +0800

    feat(style): add dark style color

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -5,27 +5,36 @@ type ThemeKey =
   | "fontColor"
   | "themeColor"
   | "backgroundColor"
+  | "toggleThemeButtonColor"
   | "dropDownBackgroundColor"
   | "resultsBackgroundColor"
   | "resultsFontColor"
   | "resultsBannerColor";
 type ExtendedTheme = Record<ThemeKey, string>;
 declare module "styled-components" {
-  export interface DefaultTheme extends ExtendedTheme {}
+  export interface DefaultTheme extends Partial<ExtendedTheme> {}
 }
 
 export const lightTheme: DefaultTheme = {
   fontColor: "#1a2a3a",
   themeColor: "#3498db",
   backgroundColor: "#f6f6f6",
+  toggleThemeButtonColor: "#1a2a3a",
   dropDownBackgroundColor: "white",
   resultsBackgroundColor: "#1a2a3a",
   resultsFontColor: "white",
   resultsBannerColor: "#4a5a6a",
 };
 //TODO perfect dark theme
-export const darkTheme: Partial<DefaultTheme> = {};
+export const darkTheme: Partial<DefaultTheme> = {
+  fontColor: "#fffafa",
+  backgroundColor: "#121212",
+  toggleThemeButtonColor: "white",
+  dropDownBackgroundColor: "#646464",
+  resultsBackgroundColor: "#dddddd",
+  resultsFontColor: "#1a2a3a",
+};
 
 export const getThemeValue = (theme: DefaultTheme, key: ThemeKey): string => {
-  return theme[key] ?? lightTheme[key];
+  return theme[key] ?? lightTheme[key]!;
 };
